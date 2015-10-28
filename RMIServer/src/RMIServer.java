b0a390f1b307796dcfb76c7d6af0222ed60680d3
mbalca <mbalca@ipn.pt>
2015-10-28 23:54:30 +0000

    Added timer on RMIServer

--- RMIServer/src/RMIServer.java
+++ RMIServer/src/RMIServer.java
@@ -2,13 +2,13 @@
  * Created by mariobalca on 24-10-2015.
  */
 
-import java.net.*;
-import java.rmi.*;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Timer;
 
 public class RMIServer{
 
@@ -19,6 +19,11 @@ public class RMIServer{
             RMIImpl rmiServer = new RMIImpl(connection);
             Registry r = LocateRegistry.createRegistry(7000);
             r.rebind("rmi", rmiServer);
+            Timer t = new Timer();
+            MyTask mTask = new MyTask();
+            // This task is scheduled to run every 10 seconds
+
+            t.scheduleAtFixedRate(mTask, 0, 10000);
             System.out.println("RMI Server ready.");
         } catch (RemoteException re) {
             System.out.println("Exception in RMIServer.main: " + re);
