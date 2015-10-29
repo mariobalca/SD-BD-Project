9296754ed3fc9ffd7d68d8809232805c952aee01
mbalca <mbalca@ipn.pt>
2015-10-29 00:18:45 +0000

    Added MyTask of RMI

---
+++ RMIServer/src/MyTask.java
@@ -0,0 +1,17 @@
+import java.util.TimerTask;
+
+/**
+ * Created by mariobalca on 28-10-2015.
+ */
+class MyTask extends TimerTask {
+
+    public MyTask(){
+        //Some stuffs
+    }
+
+
+    public void run() {
+        System.out.println("Hi see you after 10 seconds");
+    }
+
+}
