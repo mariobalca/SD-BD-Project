a1cf58c600f0259824cd01ab9e26ddee611670f3
Rui Fonseca <rui.m.c.fonseca@hotmail.com>
2015-10-31 22:54:26 +0000

    added file configuration

---
+++ GenericClasses/RewardsResponse.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+/**
+ * Created by pedro on 31-10-2015.
+ */
+public class RewardsResponse extends Response{
+
+    public ArrayList<Reward> rewards;
+    public RewardsResponse(String response, ArrayList<Reward> rewards) {
+        super(response);
+        this.rewards = rewards;
+    }
+}
