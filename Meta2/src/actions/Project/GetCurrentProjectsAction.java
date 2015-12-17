1a86707bc0c7408095ec0e1262807577f72c017e
Pedro Belém <pedrocb_c-s@hotmail.com>
2015-12-17 23:44:03 +0000

    GetCurrentProjectsAction

--- Meta2/src/actions/Project/GetCurrentProjectsAction.java
+++ Meta2/src/actions/Project/GetCurrentProjectsAction.java
@@ -11,10 +11,22 @@ import java.util.ArrayList;
  * Created by pedrocb on 17/12/2015.
  */
 public class GetCurrentProjectsAction extends ActionSupport{
-    ArrayList<Project> projects;
+    public ArrayList<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(ArrayList<Project> projects) {
+        this.projects = projects;
+    }
+
+    ArrayList<Project> projects = new ArrayList<Project>();
     public String execute(){
         ProjectRepository projectRepository = new ProjectRepository();
         projects = projectRepository.getCurrentProjects();
+        System.out.println("OLA");
+        for(Project project: projects){
+            System.out.println(project);
+        }
         return SUCCESS;
     }
 }
