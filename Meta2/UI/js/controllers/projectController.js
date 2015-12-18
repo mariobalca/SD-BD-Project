d81498c771b6cbb1be04294d98209e98f6c4d5c6
Mário Balça <mario.balca@gmail.com>
2015-12-18 05:35:11 +0000

    Added buttons and modals on dashboard

--- Meta2/UI/js/controllers/projectController.js
+++ Meta2/UI/js/controllers/projectController.js
@@ -1,5 +1,6 @@
-myApp.controller('projectController', ['$scope', '$log', '$http', '$routeParams', function($scope, $log, $http, $routeParams){
+myApp.controller('projectController', ['$scope', '$log', '$http', '$routeParams', 'authService', function($scope, $log, $http, $routeParams, authService){
 	var id = $routeParams.id;
+	$scope.loggedIn = authService.loggedIn;
 	$http.post('http://localhost:8080/api/getProject', {'projectId': id}).success(function(result){
 		$scope.project = result.project;
 		$log.info(result.project);
